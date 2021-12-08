9987b28248c483bccd7e267f02312e3a9a13f15e
domwitt21 <60725257+domwitt21@users.noreply.github.com>
2021-12-07 19:26:08 -0500

    Responsive update
    Added mobile menu for easier navigation and cleaner UI.

---
+++ js/testMobile.js
@@ -0,0 +1,5 @@
+document.getElementById('mobile-menu').addEventListener('mouseover', mouseOver);
+
+function mouseOver() {
+    document.getElementById('mobile-menu-list').style.display = 'block';
+}
